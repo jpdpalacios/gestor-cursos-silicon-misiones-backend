109f0ab1f51d10e0d35f69792cea68f8a456d394
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-04-25 01:03:46 -0300

    add: student index route and its controller

--- src/api/Controllers/student/studentController.js
+++ src/api/Controllers/student/studentController.js
@@ -1,7 +1,24 @@
+const DB = require('../../../database/models')
+const Op = DB.Sequelize.Op;
+
 const studentController = {
-    index: (req, res) => {
-        res.json('Hello i`m student!');
-    },
+    index: async (req, res) => {
+        try {
+            let alumnos = await DB.Alumno.findAll();
+
+            return res.status(200).json({
+                total: alumnos.length,
+                data: alumnos,
+                status: 200
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({
+                message: "Ocurrió un error al listar los alumnos",
+                error: error,
+            });
+        }
+    }
 }
 
 module.exports = studentController
--- src/api/api.routes.js
+++ src/api/api.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('./Controllers/course/courseController')
-//const studentController = require('./Controllers/course/studentController')
-//const userController = require('./Controllers/course/userController')
+const studentController = require('./Controllers/student/studentController')
+//const userController = require('./Controllers/user/userController')
 
 router.get('/courses', courseController.index)
+
+router.get('/students', studentController.index)
 
 module.exports = router;
