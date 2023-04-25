c45c43b2fd1a31c87b3fbdfa10946c4f6cb4ca49
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-04-25 01:33:45 -0300

    add: student creation route and its controller

--- src/api/Controllers/student/studentController.js
+++ src/api/Controllers/student/studentController.js
@@ -18,7 +18,28 @@ const studentController = {
                 error: error,
             });
         }
-    }
+    },
+    create: async (req, res) => {
+        try {
+            let { email, rol, nombre, apellido, dni } = req.body;
+            let usuario = await DB.Usuario.create({ email, rol });
+            let alumno = await DB.Alumno.create({ nombre, apellido, dni, id_usuario: usuario.id });
+
+            return res.status(200).json({
+                message: 'Alumno creado correctamente',
+                data: {
+                    usuario: usuario,
+                    alumno: alumno
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({
+                message: "Ocurrió un error al crear el alumno",
+                error: error,
+            });
+        }
+    },
 }
 
 module.exports = studentController
