7e8f9276ebbe38a85f592e8de6cb34d82f0f78fb
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-05-22 14:15:37 -0300

    mod: [api.routes.js]

--- src/api/api.routes.js
+++ src/api/api.routes.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const router = express.Router();
+
+const userExtractor = require('./middlewares/userExtractor')
+
 const courseController = require('./Controllers/course/courseController')
 const studentController = require('./Controllers/student/studentController')
 const imageController = require('./Controllers/media/imageController')
-//const userController = require('./Controllers/user/userController')
+const userController = require('./Controllers/user/userController')
 
 router.get('/courses', courseController.index)
-router.post('/courses/create', courseController.create)
-router.delete('/courses/delete/:id', courseController.delete)
-router.put('/courses/update/:id', courseController.update)
+router.post('/courses/create', userExtractor, courseController.create)
+router.delete('/courses/delete/:id', userExtractor, courseController.delete)
+router.put('/courses/update/:id', userExtractor, courseController.update)
 
-router.get('/students', studentController.index)
-router.post('/students/create', studentController.create)
-router.put('/students/update/:id', studentController.update)
-router.delete('/students/delete/:id', studentController.delete)
+router.get('/students', userExtractor, studentController.index)
+router.post('/students/create', userExtractor, studentController.create)
+router.put('/students/update/:id', userExtractor, studentController.update)
+router.delete('/students/delete/:id', userExtractor, studentController.delete)
 
-//router.post('/users/login', userController.login)
+router.post('/users/create', userController.create)
+router.post('/users/login', userController.login)
 
 router.get('/media/image/course/:id', imageController.showImageCourse)
 
