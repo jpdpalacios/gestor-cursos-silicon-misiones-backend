bba76c30fbb4d9ee0ee7dfafb9bf23177fe6b3d2
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-05-22 14:19:45 -0300

    mod: [courseController.js] improve image saving

--- src/api/Controllers/course/courseController.js
+++ src/api/Controllers/course/courseController.js
@@ -33,53 +33,53 @@ const CourseController = {
         }
     },
     create: async (req, res) => {
-        let pathCourse = path.join(__dirname, "./../../../../public/images/courses/");
-        let storage = multer.diskStorage({
-            destination: pathCourse,
-            filename: function (req, file, cb) {
-                cb(
-                    null,
-                    file.fieldname + "-" + Date.now() + "-" + file.originalname 
-                );
-            },
-        });
-        let upload = multer({ storage: storage }).single("imagen");
-        upload(req, res, async function (err) {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    message: "Ocurrió un error al subir la imagen",
-                    error: err,
-                });
-            }
+      let pathCourse = path.join(__dirname, "./../../../../public/images/courses/");
+      let storage = multer.diskStorage({
+        destination: pathCourse,
+        filename: function (req, file, cb) {
+          cb(
+            null,
+            file.fieldname + "-" + Date.now() + "-" + file.originalname 
+          );
+        },
+      });
+      let upload = multer({ storage: storage }).single("imagen");
+      upload(req, res, async function (err) {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Ocurrió un error al subir la imagen",
+            error: err,
+          });
+        }
     
-            try {
-                let courseData = req.body;
-                if (!courseData.anio) {
-                    const now = new Date();
-                    courseData.anio = now.getFullYear();
-                }
-                if (req.file) {
-                    courseData.imagen = req.file.filename;
-                }
-                let course = await DB.Curso.create(courseData);
+        try {
+          let courseData = req.body;
+          if (!courseData.anio) {
+            const now = new Date();
+            courseData.anio = now.getFullYear();
+          }
+          if (req.file) {
+            courseData.imagen = "http://localhost:8080/api/media/image/course/" + req.file.filename;
+          }
+          let course = await DB.Curso.create(courseData);
     
-                return res.status(200).json({
-                    data: course,
-                    status: 200,
-                    message: "El curso ha sido creado exitosamente",
-                })
-            } catch (error) {
-                console.error(error);
-                return res.status(500).json({
-                    message: "Ocurrió un error al crear el curso",
-                    error: error,
-                });
-            }
-        })
-    },
+          return res.status(200).json({
+            data: course,
+            status: 200,
+            message: "El curso ha sido creado exitosamente",
+          })
+        } catch (error) {
+          console.error(error);
+          return res.status(500).json({
+            message: "Ocurrió un error al crear el curso",
+            error: error,
+          });
+        }
+      })
+    }, 
     
-
+    
     update: async (req, res) => {
         let id_curso = req.params.id;
         let datos_curso = req.body;
@@ -98,7 +98,7 @@ const CourseController = {
             filename: function (req, file, cb) {
               cb(
                 null,
-                "http://localhost:8080/api/media/image/course/" + file.fieldname + "-" + Date.now() + "-" + file.originalname
+                file.fieldname + "-" + Date.now() + "-" + file.originalname
               );
             },
           });
@@ -149,7 +149,7 @@ const CourseController = {
           });
         }
       },
-          delete: async (req, res) => {
+      delete: async (req, res) => {
         try {
             const curso = await DB.Curso.findOne({ where: { id: req.params.id } });
             if (!curso) {
@@ -157,6 +157,15 @@ const CourseController = {
                     message: "No se encontró el curso con el ID especificado",
                 });
             }
+    
+            let pathCourse = path.join(__dirname, "./../../../../public/images/courses/");
+            if (curso.imagen) {
+              let pathImagen = path.join(pathCourse, curso.imagen.substring("http://localhost:8080/api/media/image/course/".length));
+              if (fs.existsSync(pathImagen)) {
+                    fs.unlinkSync(pathImagen);
+                }
+            }
+    
             await curso.destroy();
             return res.status(200).json({
                 message: "El curso ha sido eliminado exitosamente",
